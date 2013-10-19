5dbc1a958b9645eec1e3df1f28667f8fa8f299f1
giboow <philippe.gibert@gmail.com>
2013-10-19 15:59:06 +0200

    modify users view

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -4,6 +4,22 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(
+        new Silex\Provider\DoctrineServiceProvider(),
+        array(
+                'db.options' => array(
+                        'driver'    => 'pdo_mysql',
+                        'host'      => '192.168.1.47',
+                        'port'		=> '5700',
+                        'dbname'    => 'pariouvert',
+                        'user'      => 'pariouvert',
+                        'password'  => 'pariouvert',
+                        'charset'   => 'utf8',
+                ),
+        )
+);
+
+
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
         'security.firewalls' => array(
             'secured_area' => array(
@@ -22,8 +38,17 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
+
 $userServiceProvider = new SimpleUser\UserServiceProvider();
 $app->register($userServiceProvider);
+$app['user.controller']->setTemplates(
+    array(
+        "login" => "user/login.twig",
+        "register" => "user/register.twig"
+    )
+);
+
+
 
 $app->register(
     new Silex\Provider\SessionServiceProvider()
@@ -39,20 +64,7 @@ $app->register(
     new Silex\Provider\RememberMeServiceProvider()
 );
 
-$app->register(
-    new Silex\Provider\DoctrineServiceProvider(),
-    array(
-        'db.options' => array(
-            'driver'    => 'pdo_mysql',
-            'host'      => '192.168.1.47',
-            'port'		=> '5700',
-            'dbname'    => 'pariouvert',
-            'user'      => 'pariouvert',
-            'password'  => 'pariouvert',
-            'charset'   => 'utf8',
-        ),
-    )
-);
+
 
 $app->mount('/user', $userServiceProvider);
 
