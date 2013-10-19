ea89311ec56a0443a11e6ce832838d7fff3fc457
giboow <philippe.gibert@gmail.com>
2013-10-19 14:19:55 +0200

    ajout gestion utilisateurs

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -4,12 +4,48 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+        'security.firewalls' => array(
+            'secured_area' => array(
+                'pattern' => '^.*$',
+                'anonymous' => true,
+                'form' => array(
+                    'login_path' => '/user/login',
+                    'check_path' => '/user/login_check',
+                 ),
+                 'logout' => array(
+                     'logout_path' => '/user/logout',
+                ),
+                'users' => $app->share(function($app) { return $app['user.manager']; }
+            ),
+        ),
+    ),
+));
+
+$userServiceProvider = new SimpleUser\UserServiceProvider();
+$app->register($userServiceProvider);
+
+$app->register(
+    new Silex\Provider\SessionServiceProvider()
+);
+$app->register(
+    new Silex\Provider\ServiceControllerServiceProvider()
+);
+$app->register(
+    new Silex\Provider\UrlGeneratorServiceProvider()
+);
+
+$app->register(
+    new Silex\Provider\RememberMeServiceProvider()
+);
+
 $app->register(
     new Silex\Provider\DoctrineServiceProvider(),
     array(
         'db.options' => array(
             'driver'    => 'pdo_mysql',
             'host'      => '192.168.1.47',
+            'port'		=> '5700',
             'dbname'    => 'pariouvert',
             'user'      => 'pariouvert',
             'password'  => 'pariouvert',
@@ -18,6 +54,7 @@ $app->register(
     )
 );
 
+$app->mount('/user', $userServiceProvider);
 
 //CONF
 $app->register(
