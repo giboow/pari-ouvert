fbeb6b092e969501abc585d9d91c413379dff25b
samuel berthe <dev@samuel-berthe.fr>
2013-10-19 11:44:17 +0200

    index

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -2,10 +2,27 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array('db.options' => array(
+											 'driver'    => 'pdo_mysql',
+											 'host'      => '192.168.1.47',
+											 'dbname'    => 'pariouvert',
+											 'user'      => 'pariouvert',
+											 'password'  => 'pariouvert',
+											 'charset'   => 'utf8',
+											 ),
+								   ));
+
 $app = new Silex\Application();
 
-$app->get('/hello', function() {
+// page de pari
+$app->get('/pari/{type}', function($type) {
     return 'Hello!';
+
+// page point
+$app->get('/score', function() {
+    return 'Hello!';
+
 });
 
 $app->run();
