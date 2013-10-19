c2e85a75c8f15615816199bcc6711aa1de5acbc4
giboow <philippe.gibert@gmail.com>
2013-10-19 11:57:55 +0200

    delete vendors

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -4,6 +4,21 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+
+//CONF
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../src/views',
+        'debug' => true,
+));
+
+$app->get('/index', function() use ($app) {
+    return $app['twig']->render(
+            "index.twig", array()
+    );
+});
+
+
+
 $app->get('/hello', function() {
     return 'Hello!';
 });
