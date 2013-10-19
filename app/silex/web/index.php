62bd006fcc3a85b12cdcc2495379fe34003e3a4c
Sildar <sildar44@gmail.com>
2013-10-19 22:47:02 +0200

    First version of form for parking bets

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -6,6 +6,8 @@ $app = new Silex\Application();
 
 include 'config.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render(
@@ -15,8 +17,32 @@ $app->get('/', function() use ($app) {
 
 // page de pari
 $app->get('/pari/{type}', function($type) use ($app) {
-
+    $list = pariform();
+    return $app['twig']->render(
+				"formpari.twig", array("list" => $list, "user" => $app['user'])
+    );
 });
+
+$app->post('/pari/{type}', function(Request $request) use ($app){
+
+    $userId = $request->get('userid');
+    $current_date = new DateTime();
+    $current_date = $current_date->format('dd-mm-YYYY');
+    $datepari = $request->get('datepari');
+    $parkingid = $request->get('parkingid');
+    $nbplaces = $request->get('nbplaces');
+
+    $app['db']->insert('pari_parking', array(
+					     'user_id' => $userId,
+					     'parking_id' => $parkingid,
+					     'nb_place_pari' => $nbplaces,
+					     'date_pari' => $datepari,
+					     'date_create' => $current_date,
+					     
+					     ));
+    
+    return new Response("Le pari a été enregistré !", 201);
+  });
 
 // page point
 $app->get('/score', function() {
@@ -28,3 +54,9 @@ $app->get('/score/{type}', function() {
 
 
 $app->run();
+
+function pariform(){
+    global $app;
+    $p = $app['db']->fetchAll('SELECT * FROM parking');
+    return $p;
+}
