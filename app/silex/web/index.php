1eb3915355738558b32a679529094cbcb1a68417
giboow <philippe.gibert@gmail.com>
2013-10-19 16:40:42 +0200

    extract config file

--- app/silex/web/index.php
+++ app/silex/web/index.php
@@ -4,89 +4,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->register(
-        new Silex\Provider\DoctrineServiceProvider(),
-        array(
-                'db.options' => array(
-                        'driver'    => 'pdo_mysql',
-                        'host'      => '192.168.1.47',
-                        'port'		=> '5700',
-                        'dbname'    => 'pariouvert',
-                        'user'      => 'pariouvert',
-                        'password'  => 'pariouvert',
-                        'charset'   => 'utf8',
-                ),
-        )
-);
-
-
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-        'security.firewalls' => array(
-            'secured_area' => array(
-                'pattern' => '^.*$',
-                'anonymous' => true,
-                'form' => array(
-                    'login_path' => '/user/login',
-                    'check_path' => '/user/login_check',
-                 ),
-                 'logout' => array(
-                     'logout_path' => '/user/logout',
-                ),
-                'users' => $app->share(function($app) { return $app['user.manager']; }
-            ),
-        ),
-    ),
-));
-
-
-$userServiceProvider = new SimpleUser\UserServiceProvider();
-$app->register($userServiceProvider);
-$app['user.controller']->setTemplates(
-    array(
-        "login" => "user/login.twig",
-        "register" => "user/register.twig",
-        "list" => "user/list.twig",
-        "view" => "user/view.twig",
-        "edit" => "user/edit.twig",
-    )
-);
-
-
-
-$app->register(
-    new Silex\Provider\SessionServiceProvider()
-);
-$app->register(
-    new Silex\Provider\ServiceControllerServiceProvider()
-);
-$app->register(
-    new Silex\Provider\UrlGeneratorServiceProvider()
-);
-
-$app->register(
-    new Silex\Provider\RememberMeServiceProvider()
-);
-
-
-
-$app->mount('/user', $userServiceProvider);
-
-//CONF
-$app->register(
-    new Silex\Provider\TwigServiceProvider(),
-        array(
-            'twig.path' => __DIR__.'/../src/views',
-            'debug' => true,
-    )
-);
+include 'config.php';
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render(
         "index.twig", array("msg" => "hello")
     );
 });
-
-
 
 // page de pari
 $app->get('/pari/{type}', function($type) use ($app) {
